277b7d01ba87d54cf49803445215d829f5b26e19
mishra709s <mishra709s@gmail.com>
2025-06-17 00:15:12 +0530

    create react projects 2

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,5 @@
+function App() {
+  return <h1>Learn React | Soubhagya Mishra</h1>
+}
+
+export default App
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,5 @@
+function App() {
+  return <h1>Learn React with Vite | Soubhagya Mishra</h1>
+}
+
+export default App
